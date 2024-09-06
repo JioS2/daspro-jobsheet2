b1d36127f05dc0d735d43a4b0e6cdda6bad8b2bf
crezykiler <kiminsetyawan@gmail.com>
2024-09-06 14:07:06 +0700

    masukkan kode java ke github jio

---
+++ Pengenalan17.java
@@ -0,0 +1,8 @@
+public class MyFirstJavaGithub09 {
+    public static void main(String [] args) {
+        System.out.println("Nama    = Rachmad Zaki Setyawan");
+        System.out.println("Nim     = 244107060107");
+        System.out.println("Kelas   = SIB1A");
+    }
+    
+}
